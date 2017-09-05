4ba9b3db99b569139f65bbaae5102bce6fb72bff
tolja <tolja@Retina-MBP.fritz.box>
2017-09-06 01:06:54 +0200

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const https = require('https');
 const app = express()
-
+const fs = require('fs')
+const port = 3000
 
 app.use('/static', express.static(__dirname + '/assets'));
 app.use(require('./modules'));
@@ -9,6 +11,18 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.get('/add', function (req, res) {
+    res.sendFile(__dirname + '/views/add.html');
 });
+
+app.get('/rounds', function (req, res) {
+    res.sendFile(__dirname + '/views/rounds.html');
+});
+
+var privateKey = fs.readFileSync( 'key.pem' );
+var certificate = fs.readFileSync( 'cert.pem' );
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(port);
