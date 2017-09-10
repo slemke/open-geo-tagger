f9017a27f1b3dcbca95897658e7afc8813f1a185
Sascha Lemke <sascha-lemke@gmx.de>
2017-09-10 12:06:23 +0200

    added model and test files

---
+++ modules/categories/categories.model.js
@@ -0,0 +1,16 @@
+
+module.exports.get = function(limit, offset, callback) {
+
+};
+
+module.exports.insert = function(category, callback) {
+
+};
+
+module.exports.update = function(category, callback) {
+
+};
+
+module.exports.delete = function(category, callback) {
+
+};
