5f5d2c6fcd62312d6d8cde158556abf13878ac76
Sascha Lemke <sascha-lemke@gmx.de>
2017-09-15 23:22:22 +0200

    server cleanup

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express')
 const https = require('https');
 const app = express()
 const config = require('./config.js');
-const mongo = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const fs = require('fs')
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const User = require('./modules/user/user.model.js')
-const Theme = require('./modules/theme/theme.model.js')
+const auth = require('http-auth');
+
+let basic = auth.basic({
+        realm: "Users",
+    }, function(username, password, callback) {
+        callback(true); // ask mongoose db
+    }
+);
 
 //connect to MongoDB
 mongoose.connect(config.mongodb, { useMongoClient: true } );
@@ -17,24 +20,18 @@ const db = mongoose.connection;
 
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
 
-//use sessions for tracking logins
-app.use(session({
-    secret: 'opendata',
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-        mongooseConnection: db
-    })
-}));
+// use basic auth
+app.use(auth.connect(basic));
 
+// use json bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs')
+
+// setup routing for assets
 app.use('/static', express.static(__dirname + '/assets'));
+
+// use modules
 app.use(require('./modules'));
 
 // set base file for frontend output
@@ -46,44 +43,6 @@ app.get('/', function (req, res) {
       });
 });
 
-app.get('/add', function (req, res,next) {
-  
-    
-    
-User.findById(req.session.userId)
-    .exec(function (error, user) {
-
-        if (error)
-            return next(error);
-
-        if (user === null) {
-
-            var err = new Error('Not authorized! Go back!');
-            err.status = 400;
-            return next(err);
-
-        } else {
-
-            var themeID = "59b9460104b980df75c8d67e";
-            
-           res.render('add', {
-          user : user,
-        themeID : themeID
-      });
-        }
-    });
-    
-    
-    
-});
-
-app.get('/rounds', function (req, res) {
-   var userID = req.session.userId;
-      res.render('rounds', {
-          userID : userID
-      });
-});
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('File Not Found');
@@ -91,26 +50,18 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-var privateKey = fs.readFileSync( 'key.pem' );
-var certificate = fs.readFileSync( 'cert.pem' );
-
 // error handler
 // define as the last app.use callback
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err.message);
+    res.status(err.status || 500);
+    res.send(err.message);
 });
 
-// connect to mongo db
-mongo.connect(config.mongodb, function(err, db) {
-    if(err) throw err;
+// setup server
+const privateKey = fs.readFileSync( 'key.pem' );
+const certificate = fs.readFileSync( 'cert.pem' );
 
-    // store mongo db object globally
-    app.locals.db = db;
-
-    https.createServer({
-        key: privateKey,
-        cert: certificate
-    }, app).listen(config.port);
-
-});
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(config.port);
