5d44a60b97070fadd41f307810852ec8ebb36b92
Sascha Lemke <sascha-lemke@gmx.de>
2017-09-16 12:28:02 +0200

    status code improvements

--- modules/ranking/ranking.js
+++ modules/ranking/ranking.js
@@ -5,7 +5,15 @@ const model = require('../user/user.model.js')
 router.get('/', function(request, response) {
 
     model.get({}, null, null, { points: -1 }, function(err, result) {
-        response.json(docs);
+        if(!err)
+            response.status(200);
+        else
+            response.status(500);
+
+        if(!result)
+            response.status(404).end();
+        else
+            response.json(result);
     });
 });
 
