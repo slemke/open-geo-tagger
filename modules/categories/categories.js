9eded7cebb67f6ccaef91f143a036d80fe6890a8
Sascha Lemke <sascha-lemke@gmx.de>
2017-09-17 21:00:29 +0200

    fixed naming inconsistency

--- modules/categories/categories.js
+++ modules/categories/categories.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const category = require('./categories.model.js');
+const model = require('./categories.model.js');
 const auth = require('http-auth');
 const basic = require('../../auth.js');
 
 router.get('/', auth.connect(basic), function(request, response) {
 
-    category.get({}, null, null, {_id: 1}, function(err, result) {
+    model.get({}, null, null, {_id: 1}, function(err, result) {
         if(!err)
             response.status(200);
         else
@@ -24,7 +24,7 @@ router.post('/', auth.connect(basic), function(request, response) {
 
     const name = request.body.name;
 
-    category.insert({ name: name }, function(err, result) {
+    model.insert({ name: name }, function(err, result) {
         if(!err)
             response.status(201);
         else
@@ -38,7 +38,7 @@ router.get('/:id', auth.connect(basic), function(request, response) {
 
     const id = request.params.id;
 
-    category.get({ "_id" : id }, null, null, null, function(err, result) {
+    model.get({ "_id" : id }, null, null, null, function(err, result) {
         if(!err)
             response.status(200);
         else
@@ -57,7 +57,7 @@ router.put('/:id', auth.connect(basic), function(request, response) {
     const name = request.body.name;
     const update = { name: name};
 
-    category.update(id, update, function(err, result) {
+    model.update(id, update, function(err, result) {
         if(!err)
             response.status(200);
         else
@@ -71,7 +71,7 @@ router.delete('/:id', auth.connect(basic), function(request, response) {
 
     const id = request.params.id;
 
-    category.delete(id, function(err, result) {
+    model.delete(id, function(err, result) {
         if(!err)
             response.status(200);
         else
