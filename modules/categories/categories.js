85c135cd0f3bb38d0b688793843317a484604346
Sascha Lemke <sascha-lemke@gmx.de>
2017-09-17 21:25:17 +0200

    added categorie start and end query filter

--- modules/categories/categories.js
+++ modules/categories/categories.js
@@ -7,13 +7,34 @@ const basic = require('../../auth.js');
 router.get('/', auth.connect(basic), function(request, response) {
 
     const name = request.query.name;
+    let start = request.query.start;
+    let end = request.query.end;
 
     let filter = {};
 
     if(name != undefined && name != '')
         filter = { name:  new RegExp(name) };
 
-    model.get(filter, null, null, {_id: 1}, function(err, result) {
+    if(start != undefined && start != '')
+        start = parseInt(start, 10);
+    else
+        start = 0;
+
+    if(start < 0)
+        start = 0;
+
+    if(end != undefined && end != '')
+        end = parseInt(end, 10);
+    else
+        end = 40;
+
+    if(end > start) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    model.get(filter, start, end, {_id: 1}, function(err, result) {
         if(!err)
             response.status(200);
         else
