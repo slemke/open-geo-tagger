df41501d38271f590133272691bd19c65467afaa
tolja <>
2017-09-24 01:28:12 +0200

    old jquery functions for register and login

--- assets/js/global/register.js
+++ assets/js/global/register.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.input-group input[name=username]').on('focusout', function() {
+    $('.input-group input[name=username]').on('focusout', function () {
 
-        var $formVar =  $('.input-group input[name=username]');
+        var $formVar = $('.input-group input[name=username]');
 
-        if($formVar.val().length == 0 ) {
+
+        if ($formVar.val().length == 0) {
             var $form = $formVar.closest('form'),
-            $group = $formVar.closest('.input-group'),
-            $addon = $group.find('.input-group-addon'),
-            $icon = $addon.find('span');
+                $group = $formVar.closest('.input-group'),
+                $addon = $group.find('.input-group-addon'),
+                $icon = $addon.find('span');
 
             $addon.removeClass('success');
             $addon.addClass('danger');
@@ -17,45 +18,50 @@ $(document).ready(function() {
             $('.input-group input[name=username]').removeClass('validated');
 
         } else {
-            var data = { username: $("#usernameone").val()}
 
+            var data = {
+                username: $("#usernameone").val()
+            }
             $.ajax({
                 url: "https://localhost:3000/user/search",
                 method: "POST",
                 data: data
-            }).done(function(response) {
+            }).done(function (response) {
+
 
                 var $form = $formVar.closest('form'),
-                $group = $formVar.closest('.input-group'),
-                $addon = $group.find('.input-group-addon'),
-                $icon = $addon.find('span');
+                    $group = $formVar.closest('.input-group'),
+                    $addon = $group.find('.input-group-addon'),
+                    $icon = $addon.find('span');
 
                 $addon.removeClass('success');
                 $addon.addClass('danger');
                 $icon.attr('class', 'glyphicon glyphicon-remove');
                 $('.input-group input[name=username]').removeClass('validated');
 
-            }).fail(function(jqXHR, textStatus, errorThrown) {
 
-                if(jqXHR.status == 404 && $formVar.val().length > 0) {
+
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status == 404 && $formVar.val().length > 0) {
 
                     var $form = $formVar.closest('form'),
-                    $group = $formVar.closest('.input-group'),
-                    $addon = $group.find('.input-group-addon'),
-                    $icon = $addon.find('span');
+                        $group = $formVar.closest('.input-group'),
+                        $addon = $group.find('.input-group-addon'),
+                        $icon = $addon.find('span');
 
                     $addon.removeClass('danger');
                     $addon.addClass('success');
                     $icon.attr('class', 'glyphicon glyphicon-ok');
                     $('.input-group input[name=username]').addClass('validated');
 
+
                 } else if (jqXHR.status == 404 && $formVar.val().length < 1) {
 
-                    var formVar =  $('.input-group input[name=username]');
+                    var formVar = $('.input-group input[name=username]');
                     var $form = formVar.closest('form'),
-                    $group = formVar.closest('.input-group'),
-                    $addon = $group.find('.input-group-addon'),
-                    $icon = $addon.find('span');
+                        $group = formVar.closest('.input-group'),
+                        $addon = $group.find('.input-group-addon'),
+                        $icon = $addon.find('span');
 
                     $addon.removeClass('success');
                     $addon.addClass('danger');
@@ -66,25 +72,29 @@ $(document).ready(function() {
         }
     });
 
-    $('.checkloggedin').on('click', function (e) {
 
+
+    $('.checkloggedin').on('click', function (e) {
         e.stopPropagation();
         e.preventDefault();
 
-        if ( $('.dropdown').find('.dropdown-menu').is(":hidden")) {
+        if ($('.dropdown').find('.dropdown-menu').is(":hidden")) {
             $('.dropdown').find('[data-toggle=dropdown]').dropdown('toggle');
         }
     });
 
-    $('.input-group input[name=email]').on('keyup change', function() {
+
+
+
+
+    $('.input-group input[name=email]').on('keyup change', function () {
 
         var $formVar = $('.input-group input[name=email]');
-
         var $form = $(this).closest('form'),
-        $group = $(this).closest('.input-group'),
-        $addon = $group.find('.input-group-addon'),
-        $icon = $addon.find('span'),
-        state = false;
+            $group = $(this).closest('.input-group'),
+            $addon = $group.find('.input-group-addon'),
+            $icon = $addon.find('span'),
+            state = false;
 
         if (!$group.data('validate')) {
             state = $(this).val() ? true : false;
@@ -94,27 +104,29 @@ $(document).ready(function() {
 
         if (state) {
 
-            var data = { email: $("#email").val() }
+            var data = {
+                email: $("#email").val()
+            };
 
             $.ajax({
                 url: "https://localhost:3000/user/search",
                 method: "POST",
                 data: data
-            }).done(function(response) {
+            }).done(function (response) {
 
                 $addon.removeClass('success');
                 $addon.addClass('danger');
                 $icon.attr('class', 'glyphicon glyphicon-remove');
                 $('.input-group input[name=email]').removeClass('validated');
 
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-
-                if(jqXHR.status == 404 && $formVar.val().length > 0) {
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status == 404 && $formVar.val().length > 0) {
 
                     $addon.removeClass('danger');
                     $addon.addClass('success');
                     $icon.attr('class', 'glyphicon glyphicon-ok');
                     $('.input-group input[name=email]').addClass('validated');
+
 
                 } else if (jqXHR.status == 404 && $formVar.val().length < 1) {
 
@@ -122,6 +134,7 @@ $(document).ready(function() {
                     $addon.addClass('danger');
                     $icon.attr('class', 'glyphicon glyphicon-remove');
                     $('.input-group input[name=email]').removeClass('validated');
+
                 }
             });
 
@@ -131,18 +144,18 @@ $(document).ready(function() {
             $icon.attr('class', 'glyphicon glyphicon-remove');
             $('.input-group input[name=email]').removeClass('validated');
         }
+
     });
 
 
-    $(".input-group input[name=passwordconfirm]").keyup(function() {
+    $(".input-group input[name=passwordconfirm]").keyup(function () {
 
         var $form = $(this).closest('form'),
-        $group = $(this).closest('.input-group'),
-        $addon = $group.find('.input-group-addon'),
-        $icon = $addon.find('span');
+            $group = $(this).closest('.input-group'),
+            $addon = $group.find('.input-group-addon'),
+            $icon = $addon.find('span');
 
-        if($("#passwordone").val() != $("#passwordconfirm").val()) {
-
+        if ($("#passwordone").val() != $("#passwordconfirm").val()) {
             $addon.removeClass('success');
             $addon.addClass('danger');
             $icon.attr('class', 'glyphicon glyphicon-remove');
@@ -157,11 +170,78 @@ $(document).ready(function() {
         }
     });
 
-    $("#registerForm").submit(function() {
-        if($('.input-group input[name=passwordconfirm]').hasClass('validated') && $('.input-group input[name=username]').hasClass('validated') && $('.input-group input[name=email]').hasClass('validated')) {
-            return true;
+
+
+    $("#submit").on("click", function (e) {
+
+        e.preventDefault();
+
+        if ($('.input-group input[name=passwordconfirm]').hasClass('validated') && $('.input-group input[name=username]').hasClass('validated') && $('.input-group input[name=email]').hasClass('validated')) {
+
+
+            var data = {
+                email: $('#email').val(),
+                password: $('#passwordone').val(),
+                passwordconfirm: $('#passwordconfirm').val(),
+                username: $('#usernameone').val()
+            };
+
+            $.ajax({
+                type: "POST",
+                url: "https://localhost:3000/user/register",
+                data: data,
+                success: function (response) {
+                    window.location = 'https://localhost:3000/add';
+                },
+                error: function () {
+                    alert('Es gab einen Fehler bei der Registrierungsanfrage.');
+                }
+            });
+
         } else {
             return false;
         }
+    });
+
+
+    $("#login-submit").on('click', function (e) {
+
+        e.preventDefault();
+
+        if ($("#logusername").val() == "" && $("#logpassword").val() == "") {
+            $("#logusername").attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+            $("#logpassword").attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+            return false;
+        } else if ($("#logusername").val() == "" && $("#logpassword").val() != "") {
+
+            $("#logusername").attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+            $("#logpassword").removeAttr('style');
+            return false;
+
+        } else if ($("#logpassword").val() == "" && $("#logusername").val() != "") {
+
+            $("#logpassword").attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+            $("#logusername").removeAttr('style');
+            return false;
+
+        } else {
+
+            var data = {
+                logusername: $('#logusername').val(),
+                logpassword: $('#logpassword').val()
+            };
+
+            $.ajax({
+                type: "POST",
+                url: "https://localhost:3000/user/login",
+                data: data,
+                success: function (response) {
+                    window.location = 'https://localhost:3000/add';
+                },
+                error: function () {
+                    alert('Es gab einen Fehler beim Login.');
+                }
+            });
+        };
     });
 });
