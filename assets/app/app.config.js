4a6950534cfb05dc719b98b377e8c1e3da2f38dc
Sascha Lemke <sascha-lemke@gmx.de>
2017-10-07 19:07:05 +0200

    added new config file

---
+++ assets/app/app.config.js
@@ -0,0 +1,21 @@
+
+// setup routeProvider config
+function config($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "/static/app/sections/login/login.template.html",
+        controller: "LoginController",
+        controllerAs: 'vm'
+    })
+    .when("/register", {
+        templateUrl: "/static/app/sections/register/register.template.html",
+        controller: "RegisterController"
+    })
+    .when("/map", {
+        templateUrl: "/static/app/sections/map/map.template.html"
+        //controller: "MapController"
+    })
+    .when("/logout", {
+        templateUrl: "/static/app/sections/login/login.template.html",
+        controller: "LoginController"
+    });
+}
--- assets/app/app.js
+++ assets/app/app.js
@@ -9,27 +9,6 @@ var app = angular.module("opendata",
             .config(config)
             .run(run);
 
-// setup routeProvider config 
-function config($routeProvider) {
-    $routeProvider.when("/", {
-        templateUrl: "/static/app/sections/login/login.template.html",
-        controller: "LoginController",
-        controllerAs: 'vm'
-    })
-    .when("/register", {
-        templateUrl: "/static/app/sections/register/register.template.html",
-        controller: "RegisterController"
-    })
-    .when("/map", {
-        templateUrl: "/static/app/sections/map/map.template.html"
-        //controller: "MapController"
-    })
-    .when("/logout", {
-        templateUrl: "/static/app/sections/login/login.template.html",
-        controller: "LoginController"
-    });
-}
-
 // setup run callback
 run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
 
