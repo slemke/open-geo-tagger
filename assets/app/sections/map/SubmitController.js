929990b3df4707539a93453f602b5eb4b4ce89b5
tolja <>
2017-10-10 16:01:42 +0200

    SubmitController WIP

--- assets/app/sections/map/SubmitController.js
+++ assets/app/sections/map/SubmitController.js
@@ -13,30 +13,41 @@
           vm.position = LocationService.GetCurrentAddress();
           }, 500 );
 
+          ThemesService.GetAll().then(function successCallback(response) {
 
-            ThemesService.GetAll().then(function successCallback(response) {
+          vm.themes = response;
+          vm.selectedTheme = vm.themes[0]._id;
+          $timeout( function(){
+          vm.geoPosition = LocationService.GetCurrentGeoPosition();
+        }, 2000 ).then(function() {
 
-            vm.themes = response;
-            vm.selectedTheme = vm.themes[0];
+          vm.beitragForm = {
+              location : vm.geoPosition,
+              userID : "59b7ff671d8436d6cf9be301",
+              themeID : vm.selectedTheme
+          };
 
-            vm.form = {
+        })
 
-                theme : vm.themes[0]
-            };
+
+          }).catch(function(err) {
+              console.log(err);
+          });
+
+          vm.addObject = function() {
+
+            ObjectService.Create(vm.beitragForm).then(function successCallback(response) {
+
+
+              vm.beitragForm.description = "";
+              vm.beitragForm.categories = "";
+
+              vm.markers["userMarker"+MarkerService.existingMarkerObjects.length] = MarkerService.SetUserMarker(LocationService.GetCurrentGeoPosition(),response._id);
 
             }).catch(function(err) {
                 console.log(err);
             });
 
-
-
-        })();
-
-        vm.addNewObject = function() {
-
-          ObjectService.Create(vm.form);
-            // do http magic
-            console.log('logging object');
-        };
+          };
     }
 })();
