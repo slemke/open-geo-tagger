f66f9199b455a30c6fec9f642052b4e2a8f947d4
tolja <>
2017-10-10 16:01:34 +0200

    added new methods for markerservice

--- assets/app/service/marker/marker.service.js
+++ assets/app/service/marker/marker.service.js
@@ -4,18 +4,69 @@
     angular.module('opendata')
         .factory('MarkerService', MarkerService);
 
-    MarkerService.$inject = ['$http','$q','ObjectService'];
+    MarkerService.$inject = ['$http','$q', '$compile', '$rootScope', 'leafletData', 'ObjectService'];
 
-    function MarkerService($http, $q, ObjectService) {
+    function MarkerService($http, $q, $compile, $rootScope, leafletData, ObjectService) {
 
         var service = {};
 
         service.GetExistingMarkers = GetExistingMarkers;
         service.SetInitialMarker = SetInitialMarker;
-        service.SetNewMarker = SetNewMarker;
+        service.SetUserMarker = SetUserMarker;
+        service.GetMarkerInfo = GetMarkerInfo;
+        service.BindPopupToMarker = BindPopupToMarker;
+        service.markers = [];
         service.existingMarkerObjects = [];
 
         return service;
+
+
+        function BindPopupToMarker(id, markerAddress) {
+
+          leafletData.getMarkers().then(function(markers) {
+
+            console.log(markers);
+
+              angular.forEach(markers, function(currentMarker) {
+                console.log(currentMarker);
+                  if (currentMarker.options.id == id) {
+
+
+                      var popupContent = "<div>" + markerAddress + "<br><img id='markerImage' src='https://images-na.ssl-images-amazon.com/images/I/61vWHzU8L5L._SY355_.jpg'/><br><a href='' id='popup_link' ng-click='getMarkerObject();openMarkerInfo()' target='self'>Weitere Informationen</a></div>";
+
+                      // Compile title DOM into a link function
+                      var linkFn = $compile(angular.element(popupContent));
+
+                      // Return a jQuery DOM tree fully controlled by AngularJS so that ng directives will work
+                      var popup = linkFn($rootScope);
+
+                      currentMarker.bindPopup(popup[0]).openPopup();
+
+                  }
+              });
+          });
+        }
+
+        function GetMarkerInfo(event, marker) {
+
+          angular.forEach(service.existingMarkerObjects, function(val, key) {
+
+              if(val._id == marker.leafletEvent.target.options.id) {
+
+                var geocodeService = L.esri.Geocoding.geocodeService();
+
+                  geocodeService.reverse().latlng(marker.leafletEvent.target._latlng).run(function(error, result) {
+
+                      var markerAddress = result.address.Match_addr;
+
+                    service.BindPopupToMarker(marker.leafletEvent.target.options.id, markerAddress);
+
+                  });
+                  //objectID = args.leafletEvent.target.options.id;
+              };
+          });
+
+        }
 
     function GetExistingMarkers(callback) {
 
@@ -25,17 +76,13 @@
             sequence.resolve();
             sequence = sequence.promise;
 
-            var markers = [];
-
             angular.forEach(response, function(val, key) {
 
               service.existingMarkerObjects.push(val);
 
-              var location = JSON.parse(val.location);
-
               var marker = {
-                  lat: location.lat,
-                  lng: location.lng,
+                  lat: val.location[0].lat,
+                  lng: val.location[0].lng,
                   id: val._id,
                   icon: {
                       iconUrl: '/static/css/images/marker-icon-2x-green.png',
@@ -47,13 +94,14 @@
                   }
               }
 
-          markers.push(marker);
+          service.markers.push(marker);
 
         });
 
         sequence = sequence.then(function() {
 
-          callback(markers);
+
+          callback(service.markers);
 
         });
 
@@ -85,9 +133,28 @@
 
         }
 
-        function SetNewMarker(marker) {
+        function SetUserMarker(newPosition, id) {
+
+          var marker = {
+              lat: newPosition.lat,
+              lng: newPosition.lng,
+              draggable: false,
+              id: id,
+              icon: {
+                  iconUrl: '/static/css/images/marker-icon-2x-red.png',
+                  shadowUrl: '/static/css/images/marker-shadow.png',
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                  popupAnchor: [1, -34],
+                  shadowSize: [41, 41]
+              }
+        };
+
+        service.existingMarkerObjects.push(marker);
+
+        return marker;
 
         }
-    }
+      }
 
 })();
