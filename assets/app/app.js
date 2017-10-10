26ae7a27e9380b6ca10b2eba070d7491acbcd316
tolja <>
2017-10-10 23:10:07 +0200

    removed ngdialog

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,7 +1,6 @@
 var app = angular.module("opendata",
             [
                 'ngRoute',
-                'ngDialog',
                 'ngCookies',
                 'ui-leaflet',
                 'ngTagsInput'
