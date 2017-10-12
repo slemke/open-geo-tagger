e30c457508e3bfb548cf5828af5c0967596b378f
Sascha Lemke <sascha-lemke@gmx.de>
2017-10-13 01:41:09 +0200

    updated user service

--- assets/app/map/user.service.js
+++ assets/app/map/user.service.js
@@ -10,46 +10,35 @@
 
     function UserService($http) {
 
-        var user = {};
-
         return {
-            GetAll : GetAll,
-            GetById : GetById,
-            Create : Create,
-            Update : Update,
-            Delete : Delete
+            get : function(userID) {
+                var parameter = '';
+                if(userID !== undefined && userID !== null)
+                    parameter += '/' + userID;
+
+                return $http.get('/user/' + parameter)
+                    .then(handleSuccess, handleError('Error getting all users'));
+            },
+            post : function(user) {
+                return $http.post('/user/', user)
+                    .then(handleSuccess, handleError('Error creating user'));
+            },
+            put : function(user) {
+                return $http.put('/user/' + user._id, user)
+                    .then(handleSuccess, handleError('Error updating user'));
+            },
+            delete : function(id) {
+                return $http.delete('/user/' + id)
+                    .then(handleSuccess, handleError('Error deleting user'));
+            }
         };
-
-        function GetAll() {
-            return $http.get('/user/').then(handleSuccess, handleError('Error getting all users'));
-        }
-
-        function GetById(id) {
-            return $http.get('/user/' + id).then(handleSuccess, handleError('Error getting user by id'));
-        }
-
-        function Create(user) {
-            return $http.post('/user/', user).then(handleSuccess, handleError('Error creating user'));
-        }
-
-        function Update(user) {
-            return $http.put('/user/' + user._id, user).then(handleSuccess, handleError('Error updating user'));
-        }
-
-        function Delete(id) {
-            return $http.delete('/user/' + id).then(handleSuccess, handleError('Error deleting user'));
-        }
-
-        // private functions
 
         function handleSuccess(res) {
             return res.data;
         }
 
         function handleError(error) {
-            return function () {
-                return { success: false, message: error };
-            };
+            return { success: false, message: error };
         }
     }
 })();
